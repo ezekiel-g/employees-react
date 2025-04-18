bb706f28e1192323190bf6209d52b17ed41ab879
ezekiel-g <40302800+ezekiel-g@users.noreply.github.com>
2025-04-18 02:46:23 -0400

    First commit

---
+++ src/react/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <h1>This is App.js</h1>
+
+            <button onClick={() => setCount(count => count + 1)}>
+                Count is {count}
+            </button>                
+        </>
+    )
+}
+
+export default App
